b2f020454da120a3530417d001c4bae0a841d73e
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2017-09-14 20:41:24 -0600

    changed game playing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame('https://git.io/d.js-heroku');
+    client.user.setGame('League of Leggos');
 });
 
 client.on('message', msg => {
